922db03b919d854be0b60812f26b7df1804b831e
wi1dcode <magosdk@icloud.com>
2022-08-23 16:53:16 +0200

    fetch all messages

--- association-frontend/src/pages/Association.js
+++ association-frontend/src/pages/Association.js
@@ -1,44 +1,59 @@
 import { useState, useEffect } from "react"
 import { useParams, Link } from "react-router-dom"
 
-const Hero = () => {
+const Association = () => {
   const { slug } = useParams()
-  const [Association, setAssociation] = useState(null)
-
+  const [association, setAssociation] = useState(null)
+  const [message, setMessage] = useState([])
+  
   useEffect(() => {
     fetchAssociation()
+    fetchMessage()
   }, [])
 
   const fetchAssociation = async () => {
     const response = await fetch(`http://localhost:5000/Associations/${slug}`)
     const data = await response.json()
     setAssociation(data)
-  }
+}   
+
+  const fetchMessage = async () => {
+    const request = await fetch(`http://localhost:5000/message`)
+    const response = await request.json()
+    setMessage(response)
+}
 
   return (
     <section className="asso-page">
-     {Association !== null && (
-        <div className="asso-card">
-            <h1 className="title">{Association.name}</h1>
-            <p className="slogan">{Association.slogan}</p>
-            <img src={Association.image} className="asso-img" alt={Association.name} />
-            <p className="asso-desc">{Association.description}</p>
-        </div>
-    //   <div className="card-group">
-    //   <div className="card">
-    //       <img src={Association.image} className="card-img-top" alt={Association.name}/>
-    //       <div className="card-body">
-    //       <h5 className="card-title"><Link to={`/${Association.slug}`}>{Association.name}</Link></h5>
-    //       <p className="card-text">{Association.description}</p>
-    //       </div>
-    //       <div className="footer">
-    //       <p>{Association.slogan}</p>
-    //       </div>
-    //   </div>
-    //   </div>
+     {association !== null && (
+        <section>
+            <div className="asso-card">
+                <h1 className="title">{association.name}</h1>
+                <p className="slogan">{association.slogan}</p>
+                <img src={association.image} className="asso-img" alt={association.name} />
+                <p className="asso-desc">{association.description}</p>
+            </div>
+        </section>
      )}
+            <div className="asso-messages">
+            <h2 className="title mt-5">Messages:</h2>
+      {message.map((message) => {
+        return (
+            <>
+                <div className="asso-mes-content">
+                    <p className="m-object">Object: {message.object}</p>
+                    <p className="m-content">Content: {message.content}</p>
+                    <p className="m-date">{message.date}</p>
+
+                </div>
+            </>
+        )
+    }
+    
+    )}
+    </div>
     </section>
   )
 }
 
-export default Hero
+export default Association
